f34b34795e1fea29c4e8176291f2ad6d72dae506
vitor-dandrea <vitor.dandrea1@gmail.com>
2025-08-17 12:40:29 -0300

    0.0.6 chore: instalado phpcodesniffer e suas dependências
    refactor: código refatorado pelo codesniffer

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 
-Route::get('/about','App\Http\Controllers\HomeController@about')->name("home.about");
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
